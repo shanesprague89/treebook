9d511d30fae8151296b6a832c0d9a38db2e8530a
Shane Sprague <shanesprague89@gmail.com>
2014-07-11 10:57:33 -0400

    User first_name, last_name, and profile_name are not permitted in the model, receiving an error

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ActiveRecord::Base
     devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-    #attr_accessible :email, :password, :password_confirmation, :remember_me,
-                    #:first_name, :last_name, :profile_name
+     #:email, :password, :password_confirmation, :remember_me,
+    attr_accessible :first_name, :last_name, :profile_name
+                    
+    #has_many :statuses
+    def full_name
+        first_name + " " + last_name
+    end
 end
