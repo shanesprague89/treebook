d4369fcd5abdeac3e2a97cd54a52a9d39a2d343d
Shane Sprague <shanesprague89@gmail.com>
2014-07-12 18:01:23 -0400

    Added User SignIn functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,21 +10,17 @@ class User < ActiveRecord::Base
         first_name + " " + last_name
     end
     
+    has_many :statuses
+    
     def sign_up
         default_params.permit(:first_name, :last_name, :profile_name)
     end
-    
-    #NO FUCKING CLUE>>
-    #sign_in ( Devise::SessionsController)
-    #sign_up ( Devise::RegistrationsController )
-    #account_update ( Devise::RegistrationsController )
-    #<<NO FUCKING CLUE
     
     
     #This doesn't work anymore! Now, with Devise, you need strong parameters!     
     #attr_accessible :email, :password, :password_confirmation, :remember_me,
     #:first_name, :last_name, :profile_name
                     
-    #has_many :statuses
+    
    
 end
