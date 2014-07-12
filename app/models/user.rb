cff57abd6d313c18d2280f9cfe7f9796ecfa8d9e
Shane Sprague <shanesprague89@gmail.com>
2014-07-12 16:02:02 -0400

    hope this works

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,30 @@
 class User < ActiveRecord::Base
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable and :omniauthable
+
+    
     devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
-     #:email, :password, :password_confirmation, :remember_me,
-    attr_accessible :first_name, :last_name, :profile_name
-                    
-    #has_many :statuses
+    
     def full_name
         first_name + " " + last_name
     end
+    
+    def sign_up
+        default_params.permit(:first_name, :last_name, :profile_name)
+    end
+    
+    #NO FUCKING CLUE>>
+    #sign_in ( Devise::SessionsController)
+    #sign_up ( Devise::RegistrationsController )
+    #account_update ( Devise::RegistrationsController )
+    #<<NO FUCKING CLUE
+    
+    
+    #This doesn't work anymore! Now, with Devise, you need strong parameters!     
+    #attr_accessible :email, :password, :password_confirmation, :remember_me,
+    #:first_name, :last_name, :profile_name
+                    
+    #has_many :statuses
+   
 end
