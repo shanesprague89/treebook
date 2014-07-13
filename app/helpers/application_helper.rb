2f22cb5052f4140e47ad42b40e0901957cf5a665
Shane Sprague <shanesprague89@gmail.com>
2014-07-13 11:42:10 -0400

    added custom routes for auth and feed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    def flash_class(type)
+        case type
+        when :alert
+            "alert-danger"
+        when :notice
+            "alert-info"
+        else
+            ""
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+  end
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new', as: :login
+  end
+  devise_scope :user do
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+  end
+  
   resources :statuses
+  get 'feed', to: 'statuses#index', as: :feed
   root to: 'statuses#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
